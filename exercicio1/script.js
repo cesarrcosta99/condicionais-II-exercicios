56d0e9041ad80a81cb42f58dd5cd5713868058b4
unknown <cesarrcosta99@gmail.com>
2022-11-16 14:42:48 -0300

    Exercicio condicionais2 Feitos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-console.log("teste 1")
+const numero=Number(prompt("Insira um Numero"))
+  if(numero % 2 === 0){
+    if(numero % 3 === 0){
+        console.log("Esse numero é divisivel por 2 e por 3")
+    }else{
+        console.log("Esse numero é divisivel por 2,mas não é por 3")
+    }
+ }else{
+    console.log("Esse numero não é divisivel por 2 e nem por 3")
+ } 
+ 
+ if(numero % 2 === 0 && numero % 3 === 0){
+    console.log("Esse numero é divisivel por 2 e por 3")
+ }else if(numero % 2 === 0){
+    console.log("Esse numero é divisivel por 2,mas não é por 3")
+ }else{
+    console.log("Esse numero não é divisivel por 2 e nem por 3")
+ }
 
